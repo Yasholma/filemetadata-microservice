a40734effb58ca3463fcd680c8dc4a41c894c056
Yahaya <yasholma1@gmail.com>
2020-04-23 23:44:27 +0100

    Completed

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.use(fileUpload());
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", function (req, res) {
+    const fileToUpload = req.files.upfile;
+    const { name, mimetype: type, size } = fileToUpload;
+    return res.json({ name, type, size });
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+    console.log("Node.js listening ...");
 });
